a27a5299efaa03d053a3cc585c8fbbf2f61e2607
Bjørn Erik Jensen <bjorn.erik.jensen@nhn.no>
2022-07-15 15:12:21 +0200

    Renamed project Varselordningen -> AlvorligHendelse

---
+++ Example.Innmelding/AlvorligHendelse/Config.cs
@@ -0,0 +1,23 @@
+﻿using OpenAPI;
+using Shared.Configuration;
+using System;
+using System.Collections.Generic;
+
+namespace Example.Configuration
+{
+    class Config
+    {
+        public static string HelseIdUrl => "https://helseid-sts.test.nhn.no";
+
+        public static Uri ApiUri => SharedConfig.GetApiUri(Env.LOCAL);
+
+        public static ClientType ClientType => ClientType.Person;
+
+        public static string ClientId => "<Client id>";
+
+        public static Dictionary<string, object> Jwk => new Dictionary<string, object>
+        {
+            // Jwk key parts ...
+        };
+    }
+}
